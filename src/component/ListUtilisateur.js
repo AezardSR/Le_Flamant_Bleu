74174a7889956b5bb5f3b77b5b48897ed670f24c
AezardSR <69091066+AezardSR@users.noreply.github.com>
2022-09-20 16:54:46 +0200

    fin de journée, avancer sur listUtilistaeur

--- src/component/ListUtilisateur.js
+++ src/component/ListUtilisateur.js
@@ -19,10 +19,6 @@ export default class ListUtilisateur extends Component {
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},{ name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
@@ -44,45 +40,48 @@ export default class ListUtilisateur extends Component {
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
         { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
-
       ]
     render(
         
     ) {
         return (
-            <div>
+            <div id='list-utilisateur'>
+              <div id='search-bar'>
+                <input type="text" placeholder="Recherche"></input>
+                <button>Search</button>
+              </div>
               <div id="table-container">
                   <table>
-                      <tr>  
-                          <th>Nom</th>
-                          <th>Prénom</th>
-                          <th>Mail</th>
-                          <th>Téléphone</th>
-                          <th>Adresse</th>
-                          <th>Ville</th>
-                          <th>Code postale</th>
-                      </tr>
-                      {this.data.map((val, key) => {return (
-                        <tr key={key}>
-                          <td>{val.name}</td>
-                          <td>{val.firstname}</td>
-                          <td>{val.mail}</td>
-                          <td>{val.tel}</td>
-                          <td>{val.adress}</td>
-                          <td>{val.city}</td>
-                          <td>{val.zipCode}</td>
-                          <td><button></button></td>
-                        </tr>)})}
+                      <thead>
+                        <tr>  
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th>Mail</th>
+                            <th>Téléphone</th>
+                            <th>Adresse</th>
+                            <th>Ville</th>
+                            <th>Code postale</th>
+                            <th>action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.data.map((val, key) => {return (
+                          <tr key={key}>
+                            <td>{val.name}</td>
+                            <td>{val.firstname}</td>
+                            <td>{val.mail}</td>
+                            <td>{val.tel}</td>
+                            <td>{val.adress}</td>
+                            <td>{val.city}</td>
+                            <td>{val.zipCode}</td>
+                            <td><button>edit</button></td>
+                          </tr>)})}
+                      </tbody>
                   </table>
               </div>
+              <div id='add-button'>
+                <button>ajouter un utilisateur</button>
+              </div>  
             </div>
         )
     }
