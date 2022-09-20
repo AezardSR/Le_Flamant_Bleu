53937f9baa87ee6d8a102a2808d6ddde08705e67
AezardSR <69091066+AezardSR@users.noreply.github.com>
2022-09-20 10:42:07 +0200

    petite save

--- src/component/ListUtilisateur.js
+++ src/component/ListUtilisateur.js
@@ -4,31 +4,85 @@ import "../css/ListUtilisateur.css"
 
 export default class ListUtilisateur extends Component {
     data = [
-        { name: "Anom", firstname: "test", mail: "test@mail.fr" },
-        { name: "Megha", firstname: "truc", mail: "truc@mail.fr" },
-        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr"},
+        { name: "Anom", firstname: "test", mail: "test@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Megha", firstname: "truc", mail: "truc@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400" },
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},{ name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+        { name: "Subham", firstname: "machin", mail: "maichin@mail.fr", tel: "0603035014", adress:"21 Place Bertrand Labarre", city:"Noyon", zipCode:"60400"},
+
       ]
     render(
         
     ) {
         return (
             <div>
-                <table>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Prénom</th>
-                        <th>mail</th>
-                    </tr>
-                    {this.data.map((val, key) => {
-          return (
-            <tr key={key}>
-              <td>{val.name}</td>
-              <td>{val.firstname}</td>
-              <td>{val.mail}</td>
-            </tr>
-          )
-        })}
-                </table>
+              <div id="table-container">
+                  <table>
+                      <tr>  
+                          <th>Nom</th>
+                          <th>Prénom</th>
+                          <th>Mail</th>
+                          <th>Téléphone</th>
+                          <th>Adresse</th>
+                          <th>Ville</th>
+                          <th>Code postale</th>
+                      </tr>
+                      {this.data.map((val, key) => {return (
+                        <tr key={key}>
+                          <td>{val.name}</td>
+                          <td>{val.firstname}</td>
+                          <td>{val.mail}</td>
+                          <td>{val.tel}</td>
+                          <td>{val.adress}</td>
+                          <td>{val.city}</td>
+                          <td>{val.zipCode}</td>
+                          <td><button></button></td>
+                        </tr>)})}
+                  </table>
+              </div>
             </div>
         )
     }
