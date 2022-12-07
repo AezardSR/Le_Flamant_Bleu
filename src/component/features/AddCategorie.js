6e41006368c944fe78545efa0c8521787939dd14
Manon-POTEL <manonpotel@hotmail.fr>
2022-12-07 11:03:34 +0100

    POST fonctionnel

--- src/component/features/AddCategorie.js
+++ src/component/features/AddCategorie.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default AddCategorie = () => {
+const AddCategorie = () => {
 
-  const handleSubmit = () => {
+  const [categorie, setCategorie] = useState([]);
+
+  const handleSubmit = (event) => {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ categorie: 'React POST Request Example' })
+        body: JSON.stringify({categorie: categorie})
     };
+    console.log(categorie);
     fetch('http://localhost:8000/api/categories', requestOptions)
         .then(response => response.json())
+        .then(data => console.log(data))
+        event.preventDefault();
   }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Nom de la catégorie:
-          <input type="text" name="categorie" />
-        </label>
-        <button type="submit">Ajouter la catégorie</button>
+      <form>
+        <label>Nom de la catégorie:</label>
+        <input type="text" name="categorie" onChange={(event) => {setCategorie(event.target.value)}} />
+        <button type="submit" onClick={handleSubmit}>Ajouter la catégorie</button>
       </form>
     </div>
   )
 }
+
+export default AddCategorie;
