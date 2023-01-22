4e439e9089d639dfae0a0079f45d3794055e703d
Manon-POTEL <manonpotel@hotmail.fr>
2023-01-22 18:48:10 +0100

    Commit changement de PC

--- src/component/pages/Exercice.js
+++ src/component/pages/Exercice.js
@@ -47,7 +47,6 @@ function Exercice() {
                           );
                         })}
                       </ul>
-
                     </li>
                   ))}
               </ul>
