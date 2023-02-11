43ecb766972ec6604a348a105ae64f8c7c7840ce
Manon POTEL <manonpotel@hotmail.fr>
2023-02-11 12:08:52 +0100

    Add appointments

--- src/component/features/AddEventPlanning.js
+++ src/component/features/AddEventPlanning.js
@@ -1,26 +1,87 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-date-picker';
 import { Link } from 'react-router-dom';
 import '../../css/AddEventPlanning.css';
 
-export function AddEventPlanning() {
-    const [dateEvent, onChange] = useState(new Date());
+const AddEventPlanning = () => {
+
+    const [title, setTitle] = useState([]);
+    const [description, setDescription] = useState([]);
+    const [date, setDate] = useState([]);
+
+    const [receiver, setReceiver] = useState([]);
+    const [receiverID, setReceiverID] = useState([]);
+
+    const [created, setCreated] = useState([]);
+    const [createdID, setCreatedID] = useState([]);
+
+    const [typeAppoitments, setTypeAppoitments] = useState([]);
+    const [typeAppoitmentsID, setTypeAppoitmentsID] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/user')
+        .then(response => response.json())
+        .then(data => setReceiver(data))
+    }, [])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/user')
+        .then(response => response.json())
+        .then(data => setCreated(data))
+    }, [])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/api/appointmentstypes')
+        .then(response => response.json())
+        .then(data => setTypeAppoitments(data))
+    }, [])
+
+    const handleSubmit = (event) => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({titleDetails: title, descriptionDetails: description, dateDetails: date, receiver_id: receiverID, create_id: createdID, appointments_types_id: typeAppoitmentsID})
+        };
+
+        fetch('http://localhost:8000/api/appointments', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            event.preventDefault();
+    }
+
     return (
       <div>
 
         <form className='form-add-event-planning'>
             <div className='add-event-date'>
-                <DatePicker onChange={onChange} value={dateEvent} />
-                {/* <p>{dateEvent.toDateString()}</p> */}
+                <input value={date} onChange={(event) => {setDate(event.target.value)}} name="dateOffers" type="date"></input>
             </div>
 
             <div className='add-event-description'>
-                <input className='add-event-description-title' placeholder='Titre -'></input>
-                <textarea placeholder='Détails -'></textarea>
+                <input value={title} onChange={(event) => {setTitle(event.target.value)}} name="titleDetails" className='add-event-description-title' placeholder='Titre -'></input>
+                <textarea value={description} onChange={(event) => {setDescription(event.target.value)}} name="descriptionDetails" placeholder='Détails -'></textarea>
             </div>
 
+            <select className="p-5px w-100 h-45px" style={{marginBottom: '20px', fontSize: 'Medium'}} onChange={(event) => {setReceiverID(event.target.value)}} value={receiverID}>
+                {receiver.map((invit) => (
+                    <option key={invit.id} value={invit.id}>{invit.id} : {invit.name} {invit.firstname}</option>
+                ))}
+            </select>
+
+            <select className="p-5px w-100 h-45px" style={{marginBottom: '20px', fontSize: 'Medium'}} onChange={(event) => {setCreatedID(event.target.value)}} value={createdID}>
+                {created.map((createdBy) => (
+                    <option key={createdBy.id} value={createdBy.id}>{createdBy.id} : {createdBy.name} {createdBy.firstname}</option>
+                ))}
+            </select>
+
+            <select className="p-5px w-100 h-45px" style={{marginBottom: '20px', fontSize: 'Medium'}} onChange={(event) => {setTypeAppoitmentsID(event.target.value)}} value={typeAppoitmentsID}>
+                {typeAppoitments.map((typeRDV) => (
+                    <option key={typeRDV.id} value={typeRDV.id}>{typeRDV.id} : {typeRDV.name}</option>
+                ))}
+            </select>
+
             <div>
-                <Link to="/calendrier"><button type="submit" className="btn btn-add-event-planning">Ajouter</button></Link>
+                <Link to="/calendrier"><button onClick={handleSubmit} type="submit" className="btn btn-add-event-planning">Ajouter</button></Link>
             </div>
         </form>
 
--- src/component/pages/Planning.js
+++ src/component/pages/Planning.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react'
 import Calendar from 'react-calendar';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import '../../css/Calendar.css';
 import { Link } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 export default function Planning() {
     const [date, setDate] = useState(new Date());
+
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        Promise.all([
+          fetch('http://localhost:8000/api/appointments'),
+        ])
+          .then(([resAppointments]) =>
+            Promise.all([resAppointments.json()])  
+          )
+          .then(([dataAppointments]) => {
+            setAppointments(dataAppointments);
+          })
+      })
     return (
       <div>
             <div className='calendar-container'>
                 <Calendar onChange={setDate} value={date}  />
             </div>
-            <div className='text-center date-select-calendar'>
+            {/* <div className='text-center date-select-calendar'>
                 <p className='planning-date'>{date.toDateString()}</p>
                 <p className="date-select-formation">Cours 'Nom de la formation' de 'minHeure' à 'maxHeure' </p>
-            </div>
-            <div className='text-center date-select-calendar'>
-                <p className='planning-date'>Date de l'event ajouter</p>
-                <p className="date-select-formation planning-title">Titre de l'event ajouter</p>
-                <p className="date-select-formation planning-details">Détails de l'event ajouter</p>
-            </div>
+            </div> */}
+            {appointments.map((appointments) => (
+                <div className='text-center date-select-calendar'>
+                <p className='planning-date'>{appointments.dateDetails}</p>
+                <p className="date-select-formation planning-title">{appointments.titleDetails}</p>
+                <p className="date-select-formation planning-details">{appointments.descriptionDetails}</p>
+                </div>
+                
+            ))}
             <div className='add-event-planning date-select-calendar'>
                 <Link to='/add-event-planning'>+ Ajouter un évènement</Link>
             </div>
