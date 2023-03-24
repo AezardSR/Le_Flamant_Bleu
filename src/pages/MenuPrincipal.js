97c69398626223ec00c949d13d42f7b277e7b5b7
Manon POTEL <manonpotel@hotmail.fr>
2023-03-24 14:17:45 +0100

    Check responsive 24/03

--- src/pages/MenuPrincipal.js
+++ src/pages/MenuPrincipal.js
@@ -5,6 +5,7 @@ import LaManu from '../assets/img/logo_la_manu.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperclip ,faAddressCard, faGear, faHome, faCalendar, faGraduationCap, faLaptopCode, faMessage, faCircleQuestion, faGlobe, faAddressBook, faDoorClosed} from "@fortawesome/free-solid-svg-icons";
 import '../css/MenuPrincipal.css';
+import '../css/global.css';
 import { ApiContext, ApiProvider } from "../features/APIToken/ApiContext";
 
 
@@ -13,32 +14,36 @@ function Main() {
     console.log(user);
     return(
         <div className="menu-principal">
-            <div className="avatar-menu">
-                <img src={Avatar} alt="avatar de la personne" className="img-menu-avatar"/>
-                <p>{user.firstname}</p>
-                <div className='avatar-menu-parametre'>
-                    <Link to="/profile"><FontAwesomeIcon icon={faGear} style={{color: 'white'}} /></Link>
-                    <Link to="/login"><FontAwesomeIcon icon={faDoorClosed} style={{color: 'white'}} /></Link>
+            <div className="p-relative">
+                <div className="avatar-menu">
+                    <img src={Avatar} alt="avatar de la personne" className="img-menu-avatar"/>
+                    <p>{user.firstname}</p>
+                    <div className='avatar-menu-parametre'>
+                        <Link to="/profile"><FontAwesomeIcon icon={faGear} style={{color: 'white'}} /></Link>
+                        <Link to="/login"><FontAwesomeIcon icon={faDoorClosed} style={{color: 'white'}} /></Link>
+                    </div>
                 </div>
-            </div>
-            <div className="listing-menu">
-                <ul className="ul-listing-menu">
-                    <Link to="/"><li><FontAwesomeIcon icon={faHome} />Tableau de bord</li></Link>
-                    <Link to="/ma-formation"><li><FontAwesomeIcon icon={faGraduationCap} />Ma formation</li></Link>
-                    {/* <Link to="/calendrier"><li><FontAwesomeIcon icon={faCalendar} />Calendrier</li></Link>
-                    <Link to="/cours"><li><FontAwesomeIcon icon={faGraduationCap} />Cours</li></Link>
-                    <Link to="/exercices"><li><FontAwesomeIcon icon={faLaptopCode} />Exercices</li></Link> */}
-                    <Link to="/"><li><FontAwesomeIcon icon={faMessage} />Messagerie</li></Link>
-                    <Link to="/"><li><FontAwesomeIcon icon={faCircleQuestion} />FAQ</li></Link>
-                    <Link to="/actualites"><li><FontAwesomeIcon icon={faGlobe} />Actualités</li></Link>
-                    <Link to="/emplois"><li><FontAwesomeIcon icon={faAddressBook} />Emplois</li></Link>
-                    <Link to="/admin"><li><FontAwesomeIcon icon={faPaperclip} />Administratifs</li></Link>
-                    {/* <Link to="/annonces-emplois"><li><FontAwesomeIcon icon={faAddressBook} />Offres d'emplois</li></Link>
-                    <Link to="/list-user"><li id="menu-fiche-information"><FontAwesomeIcon icon={faAddressCard} />Fiches d'informations</li></Link> */}
-                    <a href="https://lamanu.fr/">
-                        <img src={LaManu} alt="LOGO" className='logo-menu-principal' style={{width: '70%', height:'auto'}}/>
-                    </a>
-                </ul>
+                <div className="listing-menu">
+                    <input class="menu-btn" type="checkbox" id="menu-btn" />
+                    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+                    <ul className="ul-listing-menu menu">
+                        <Link to="/"><li><FontAwesomeIcon icon={faHome} />Tableau de bord</li></Link>
+                        <Link to="/ma-formation"><li><FontAwesomeIcon icon={faGraduationCap} />Ma formation</li></Link>
+                        {/* <Link to="/calendrier"><li><FontAwesomeIcon icon={faCalendar} />Calendrier</li></Link>
+                        <Link to="/cours"><li><FontAwesomeIcon icon={faGraduationCap} />Cours</li></Link>
+                        <Link to="/exercices"><li><FontAwesomeIcon icon={faLaptopCode} />Exercices</li></Link> */}
+                        <Link to="/"><li><FontAwesomeIcon icon={faMessage} />Messagerie</li></Link>
+                        <Link to="/"><li><FontAwesomeIcon icon={faCircleQuestion} />FAQ</li></Link>
+                        <Link to="/actualites"><li><FontAwesomeIcon icon={faGlobe} />Actualités</li></Link>
+                        <Link to="/emplois"><li><FontAwesomeIcon icon={faAddressBook} />Emplois</li></Link>
+                        <Link to="/admin"><li><FontAwesomeIcon icon={faPaperclip} />Administratifs</li></Link>
+                        {/* <Link to="/annonces-emplois"><li><FontAwesomeIcon icon={faAddressBook} />Offres d'emplois</li></Link>
+                        <Link to="/list-user"><li id="menu-fiche-information"><FontAwesomeIcon icon={faAddressCard} />Fiches d'informations</li></Link> */}
+                        <a href="https://lamanu.fr/">
+                            <img src={LaManu} alt="LOGO" className='logo-menu-principal' style={{width: '70%', height:'auto'}}/>
+                        </a>
+                    </ul>
+                </div>
             </div>
         </div>
 )}
