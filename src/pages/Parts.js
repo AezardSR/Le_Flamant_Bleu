d24c837cf6d90a7caa9c3759136beacf61c344fd
Manon POTEL <manonpotel@hotmail.fr>
2023-04-18 11:46:13 +0200

    Message d'erreur pour parts

--- src/pages/Parts.js
+++ src/pages/Parts.js
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 function Parts() {
 
     const [parts, setParts] = useState([]);
+    const [messageError, setMessageError] = useState(false);
 
     useEffect(() => {
           fetch(`${process.env.REACT_APP_API_PATH}/parts`)
@@ -15,20 +16,32 @@ function Parts() {
 
     function deleteID(id) {
       fetch(`${process.env.REACT_APP_API_PATH}/parts/` + id, { method: 'DELETE' })
-        .then(response => response.json())
+        .then(response => {
+          if (response.status != 200) {
+            console.log('test');
+            setMessageError(true);
+          }
+        })
         .then(data => console.log(data))
+
+      setMessageError(false);
     }
 
     return (
       <div>
-        <div>
+        <div className='flex-wrap'>
             {parts.map((part) => (
-                <div id={part.id} key={part.id} value={part.id}>
+                <div className='date-select-calendar flex-between' id={part.id} key={part.id} value={part.id}>
                     <p>{part.name}</p>
-                    <button className='btn-delete' onClick={() => deleteID(part.id)}>Delete</button>
-                    <button type="submit" className='btn-update'><Link to={"/modifier-parties/" + part.id}>Update</Link></button>
+                    <div>
+                      <button className='' onClick={() => deleteID(part.id)}>Delete</button>
+                      <button type="submit" className='mar-left-10px'><Link to={"/modifier-parties/" + part.id}>Update</Link></button>
+                    </div>
                 </div>
             ))}
+            {messageError && (
+              <div className='error-delete'> Une erreur est survenu, veuillez effacer les exercices et cours en liant avec cette partie </div>
+            )}
         </div>
 
         <Link to="/ajouter-parties"><button className="link-lesson-add mar-bottom-10px">Ajouter une partie</button></Link>
