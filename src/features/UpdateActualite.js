c38f37655a05306ba696ece3af82639cb26351da
Manon POTEL <manonpotel@hotmail.fr>
2023-04-19 22:17:01 +0200

    Fin gestion + view => Attention, nécessaire le push de l'api !

--- src/features/UpdateActualite.js
+++ src/features/UpdateActualite.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const UpdateActualite = () => {
-
   const { actualitesID } = useParams();
 
   const [actualites, setActualites] = useState({ title: '', content: '', publication_date: '', author: '' });
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_PATH}/actualites/`)
+    fetch(`${process.env.REACT_APP_API_PATH}/actualites/${actualitesID}`)
       .then(response => response.json())
       .then(data => setActualites(data));
 
@@ -24,7 +23,7 @@ const UpdateActualite = () => {
     const requestOptions = {
         method: 'PATCH',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({actualites: actualitesID})
+        body: JSON.stringify({title: actualites.title, content: actualites.content, publication_date: actualites.publication_date, author: actualites.author})
     };
     fetch(`${process.env.REACT_APP_API_PATH}/actualites/` + actualitesID, requestOptions)
       .then(response => response.json())
@@ -34,18 +33,26 @@ const UpdateActualite = () => {
 
   return (
     <div>
-      <h1 className="mar-vertical-10px mar-left-10px">Modifier une actualité</h1>
+      <h1 className="mar-vertical-10px mar-left-10px">Modifier un article</h1>
 
       <form className="flex-column form-add" onSubmit={handleSubmit}>
         <div className="flex align-center justify-center form-add-element">
           <div className="flex-column w-500px mar-left-10px mar-vertical-10px">
-            <label className="label-form" htmlFor="actualites">Titre</label>
-            <input type="text" id="name" name="actualites" value={actualites.title} onChange={handleInputChange} />
+            <label className="label-form" htmlFor="title">Titre à modifier</label>
+            <input type="text" id="title" name="title" value={actualites.title} onChange={handleInputChange} />
+
+            <label className="label-form" htmlFor="publication_date">Date à modifier</label>
+            <input type="date" id="publication_date" name="publication_date" value={actualites.publication_date} onChange={handleInputChange} />
+            
+            <label className="label-form" htmlFor="content">Contenu à modifier</label>
+            <textarea value={actualites.content} name="content" onChange={handleInputChange}></textarea>
+
+            <label className="label-form" htmlFor="author">Auteur à modifier</label>
+            <input type="text" id="author" name="author" value={actualites.author} onChange={handleInputChange} />
           </div>
         </div>
-        <button className="w-max-content mar-left-auto link-lesson-add mar-vertical-10px pointer" type="submit">Modifier l'actualité</button>
+        <button className="w-max-content mar-left-auto link-lesson-add mar-vertical-10px pointer" type='submit'>Modifier l'article</button>
       </form>
-
     </div>
   );
 };
